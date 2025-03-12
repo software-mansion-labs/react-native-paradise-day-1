198dd869124d58582439ba2fcc76e944496e81fa
lukmccall <kosmatylukasz@gmail.com>
2025-03-12 23:11:14 +0100

    Profiling with Sentry

--- app/(mainstack)/(protected)/(tabs)/(toptabs)/index.tsx
+++ app/(mainstack)/(protected)/(tabs)/(toptabs)/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import { HomeScreen } from "@/screens/HomeScreen";
 import { useRouter } from "expo-router";
 
+import * as Sentry from "@sentry/react";
+
+const HomeWithProfiler = Sentry.withProfiler(HomeScreen, {
+  name: "Home",
+  includeRender: true,
+  includeUpdates: true,
+});
+
 export default function Home() {
   const router = useRouter();
   return (
-    <HomeScreen
+    <HomeWithProfiler
       navigateToRides={() => {
         router.push("/rides");
       }}
