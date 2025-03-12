0d7b46c6bb42a498b8c02d62d77836a021cabba6
lukmccall <kosmatylukasz@gmail.com>
2025-03-12 23:20:52 +0100

    Fetching updates manually

--- app/(mainstack)/updates.tsx
+++ app/(mainstack)/updates.tsx
@@ -2,32 +2,21 @@ import React from "react";
 import * as Updates from "expo-updates";
 
 import { UpdateManager } from "@/components/UpdateManager";
-import { delay } from "@/utils/delay";
 import { HideSplash } from "@/components/HideSplash";
 
 // TODO(updates-api): Write a function that checks for updates
 async function checkForUpdates(): Promise<Updates.UpdateCheckResult> {
-  await delay(400);
-  return {
-    isAvailable: true,
-    manifest: {
-      id: "b14c571a-2155-4578-aab3-fa42c1decf4d",
-      createdAt: "Mar 8, 2025 12:32 AM",
-    },
-  } as Updates.UpdateCheckResultAvailable;
+  return await Updates.checkForUpdateAsync();
 }
 
 // TODO(updates-api): Write a function that fetches updates
 async function fetchUpdate(): Promise<Updates.UpdateFetchResult> {
-  await delay(400);
-  return {
-    isNew: true,
-  } as Updates.UpdateFetchResultSuccess;
+  return await Updates.fetchUpdateAsync();
 }
 
 // TODO(updates-api): Write a function that restarts the app
 async function restartApp(): Promise<void> {
-  await delay(400);
+  return await Updates.reloadAsync();
 }
 
 export default function UpdatesRoute() {
